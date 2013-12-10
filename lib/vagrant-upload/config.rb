bfd5728bd209050a41724e333afc50b9256ac082
Hiroshi Miura <miurahr@linux.com>
2013-12-10 23:42:16 +0900

    add scp method
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- lib/vagrant-upload/config.rb
+++ lib/vagrant-upload/config.rb
@@ -2,14 +2,23 @@ module VagrantPlugins
   module Upload
     class Config < Vagrant.plugin("2", :config)
 
-      attr_accessor :files
+      attr_accessor :files, :method
 
       def initialize
         @files = UNSET_VALUE
+        @method = UNSET_VALUE
       end
 
       def finalize!
         @files = nil if @files == UNSET_VALUE
+        case @method
+        when UNSET_VALUE
+          @method = :rsync
+        when 'scp'
+          @method = :scp
+        else
+          @method = :rsync
+        end
       end
 
     end
--- lib/vagrant-upload/provisioner.rb
+++ lib/vagrant-upload/provisioner.rb
@@ -4,8 +4,14 @@ module VagrantPlugins
 
       def provision
         config.files.each do |args|
+        case config.method
+        when :rsync
           @machine.env.ui.info rsync_cmd(args[:host], args[:guest])
           system rsync_cmd(args[:host], args[:guest])
+        when :scp
+          @machine.env.ui.info scp_cmd(args[:host], args[:guest])
+          system scp_cmd(args[:host], args[:guest])
+        end
         end
       end
 
@@ -21,7 +27,19 @@ module VagrantPlugins
                       "-o IdentitiesOnly=yes " <<
                       "-o LogLevel=ERROR"
 
-        "rsync -av --partial #{rsync_opts} -e 'ssh #{ssh_opts}' #{host_path} :#{guest_path}"
+        "rsync -av --partial #{rsync_opts} -e 'ssh #{ssh_opts}' #{host_path} #{@machine.ssh_info[:host]}:#{guest_path}"
+      end
+
+      def scp_cmd(host_path, guest_path)
+        scp_opts   =  "-i #{@machine.ssh_info[:private_key_path]} " <<
+                      "-p #{@machine.ssh_info[:port]} " <<
+                      "-r" <<
+                      "-o StrictHostKeyChecking=no " <<
+                      "-o UserKnownHostsFile=/dev/null " <<
+                      "-o IdentitiesOnly=yes " <<
+                      "-o LogLevel=ERROR"
+
+        "scp #{scp_opts} #{host_path} #{@machine.ssh_info[:username]}@#{@machine.ssh_info[:host]}:#{guest_path}"
       end
 
     end
