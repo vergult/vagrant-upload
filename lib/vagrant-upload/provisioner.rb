517e9905b8e8aa9fc79333d93b6509f7e1dc9727
Hiroshi Miura <miurahr@linux.com>
2013-12-11 11:19:16 +0900

    expand path and files
    Signed-off-by: Hiroshi Miura <miurahr@linux.com>

--- lib/vagrant-upload/provisioner.rb
+++ lib/vagrant-upload/provisioner.rb
@@ -4,11 +4,13 @@ module VagrantPlugins
 
       def provision
         config.files.each do |args|
+          hostpath = File.expand_path(args[:host])
+          hostfiles = Dir.glob(hostpath)
           case config.method
           when :rsync
-            command = rsync_cmd(args[:host], args[:guest])
+            command = rsync_cmd(hostfiles, args[:guest])
           when :scp
-            command = scp_cmd(args[:host], args[:guest])
+            command = scp_cmd(hostfiles, args[:guest])
           end
           @machine.env.ui.info command
           res = Vagrant::Util::Subprocess.execute(*command)
@@ -23,18 +25,32 @@ module VagrantPlugins
 
       private
 
-      def rsync_cmd(host_path, guest_path)
-        rsync_opts = "--rsync-path='sudo rsync'"
+      # generate rsync command
+      #
+      # @argument host_files [Array]
+      #           guest_path [String]
+      # @return [Array]
+      def rsync_cmd(host_files, guest_path)
+        rsync_opts = "--rsync-path=sudo rsync"
         ssh_opts   = ssh_common_opts.push("-l #{@machine.ssh_info[:username]}").join(' ')
-        ["rsync","-av","--partial","#{rsync_opts}", "-e","'ssh #{ssh_opts}'",
-                 "#{host_path}", "#{@machine.ssh_info[:host]}:#{guest_path}"]
+        ["rsync","-av","--partial","#{rsync_opts}", "-e","ssh #{ssh_opts}"] +
+        host_files + ["#{@machine.ssh_info[:host]}:#{guest_path}"]
       end
 
-      def scp_cmd(host_path, guest_path)
+      # generate scp command
+      #
+      # @argument host_files [Array]
+      #           guest_path [String]
+      # @return [Array]
+      def scp_cmd(host_files, guest_path)
         ["scp"] + ssh_common_opts +
-        ["-r", "#{host_path}", "#{@machine.ssh_info[:username]}@#{@machine.ssh_info[:host]}:#{guest_path}"]
+        ["-r"] + host_files +
+        ["#{@machine.ssh_info[:username]}@#{@machine.ssh_info[:host]}:#{guest_path}"]
       end
 
+      # return generic commmon ssh options
+      #
+      # @return [Array]
       def ssh_common_opts
         ["-i #{@machine.ssh_info[:private_key_path]}",
          "-p #{@machine.ssh_info[:port]}",
